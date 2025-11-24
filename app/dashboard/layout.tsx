94b4acca357f5e83aa19c66f7764280ed4cd22bc
Armin <dzarma.busatlic@gmail.com>
2025-11-24 23:10:11 +0100

    adding clerk and convex, creating connection between clerk and convex, creating landing page, redirecting logged user to dashboard with chat. creating side bar that creates and deletes chats in convex, creating table schema

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Authenticated } from "convex/react";
+import Header from "@/components/Header";
+import Sidebar from "@/components/SideBar";
+import { NavigationProvider } from "@/lib/NavigationProvider";
+
+export default function DashboardLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <NavigationProvider>
+      <div className="flex h-screen">
+        <Authenticated>
+          <Sidebar />
+        </Authenticated>
+        <div className="flex-1">
+          <Header />
+          <main>{children}</main>
+        </div>
+      </div>
+      //{" "}
+    </NavigationProvider>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import AiChat from "@/components/AiChat";
+export default function ChatPage() {
+  return (
+    <main className="min-h-screen bg-white flex items-center justify-center p-4">
+      <AiChat />
+    </main>
+  );
+}
